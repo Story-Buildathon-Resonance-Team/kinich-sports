5f436a7c32d52340b1eaf8e7464b1e90729c0625
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-11-12 12:56:22 -0500

    set up dynamic wallet

---
+++ src/app/Web3Providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { createConfig, WagmiProvider } from "wagmi";
+import { http } from "viem";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { PropsWithChildren } from "react";
+import { aeneid } from "@story-protocol/core-sdk";
+
+// setup wagmi
+const config = createConfig({
+  chains: [aeneid],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [aeneid.id]: http(),
+  },
+});
+const queryClient = new QueryClient();
+
+export default function Web3Providers({ children }: PropsWithChildren) {
+  return (
+    // setup dynamic
+    <DynamicContextProvider
+      settings={{
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID as string,
+        walletConnectors: [EthereumWalletConnectors],
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
+  );
+}
