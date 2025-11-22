f59a4220537d49173b17781706d0bf8374263f99
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-11-22 12:59:14 -0500

    fix 404 fetch error

--- src/app/Web3Providers.tsx
+++ src/app/Web3Providers.tsx
@@ -37,9 +37,7 @@ function DynamicProviderWrapper({ children }: PropsWithChildren) {
           EthereumWalletConnectors,
           ZeroDevSmartWalletConnectors,
         ],
-        cssOverrides: (
-          <link rel='stylesheet' href='/src/styles/external-styles.css' />
-        ),
+        cssOverrides: <link rel='stylesheet' href='/external-styles.css' />,
         handlers: {
           handleAuthenticatedUser: async (args) => {
             // This fires every time a user successfully authenticates
