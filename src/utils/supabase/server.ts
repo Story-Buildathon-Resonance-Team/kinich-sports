c7c525e5b6c886947394d706ec289dd71d8a9406
CRAIG MUTEGI MUTUGI <123402581+cmm25@users.noreply.github.com>
2025-12-10 11:17:42 +0300

    Refactor Supabase client creation to use env variables
    Remove default values for Supabase URL and key.

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,9 +4,8 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://jtticmjpfpkfjeqhifts.supabase.co";
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0dGljbWpwZnBrZmplcWhpZnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NDc0ODksImV4cCI6MjA3ODUyMzQ4OX0.4th3JE9sXib-RpzeDm40LJdNOce6oFEi24g1dp0WE4M";
-
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY 
   return createServerClient(
     supabaseUrl,
     supabaseKey,
